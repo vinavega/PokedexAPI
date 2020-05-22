0e88647bff223e3677691d40022104593b0f8159
Victor Navega <victornavega@finchsolucoes.com.br>
2020-05-22 17:47:27 -0300

    some h2 config

--- src/main/java/com/dataservice/Pokedex/Pokemon/Pokemon.java
+++ src/main/java/com/dataservice/Pokedex/Pokemon/Pokemon.java
@@ -3,15 +3,20 @@ package com.dataservice.Pokedex.Pokemon;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 @ApiModel(description = "Detalhes dos pokemons")
+@Entity
 public class Pokemon {
 
+    @Id
     private int id;
+
     @Size(min = 1,max = 20)
     @ApiModelProperty(notes="Min = 1, Max = 20")
     private String name;
--- src/main/java/com/dataservice/Pokedex/Pokemon/PokemonDaoService.java
+++ src/main/java/com/dataservice/Pokedex/Pokemon/PokemonDaoService.java
@@ -1,12 +1,26 @@
 package com.dataservice.Pokedex.Pokemon;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@Repository
+@Transactional
 public class PokemonDaoService {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public long insert(Pokemon poke){
+        entityManager.persist(poke);
+        return poke.getId();
+    }
     private static final List<Pokemon> pokemons = new ArrayList<>();
 
     static {
